965c9d6021919a799b95926f218e631150bae55e
Ilya Partanskiy <partanskiy.im@gmail.com>
2026-07-21 15:57:32 +0300

    refactor: log to stderr only, drop journald integration

--- src/log_setup.rs
+++ src/log_setup.rs
@@ -4,19 +4,8 @@ pub fn init() {
     let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let fmt_layer = fmt::layer().with_target(false).with_writer(std::io::stderr);
 
-    let registry = tracing_subscriber::registry().with(filter).with(fmt_layer);
-
-    #[cfg(target_os = "linux")]
-    let registry = registry.with(if running_under_systemd() {
-        tracing_journald::layer().ok()
-    } else {
-        None
-    });
-
-    registry.init();
-}
-
-#[cfg(target_os = "linux")]
-fn running_under_systemd() -> bool {
-    std::env::var_os("INVOCATION_ID").is_some() || std::env::var_os("JOURNAL_STREAM").is_some()
+    tracing_subscriber::registry()
+        .with(filter)
+        .with(fmt_layer)
+        .init();
 }
